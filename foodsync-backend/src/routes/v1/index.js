4291eb0a8084efdd9f4b20f69e18bdb1c0de85fb
Reudys <reudysfernando@gmail.com>
2026-02-05 08:22:53 -0400

    feat: Add .gitignore configuration and employee module implementation

--- foodsync-backend/src/routes/v1/index.js
+++ foodsync-backend/src/routes/v1/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 const router = express.Router();
 
 import menuRoutes from './menu.routes.js';
+import employeeRoutes from './employee.routes.js';
 
 router.get('/health', (req, res) => {
     res.status(200).json({ status: 'success', message: 'API is healthy' });
 });
 
 router.use('/menu', menuRoutes);
+router.use('/employee', employeeRoutes);
 
 export default router;
